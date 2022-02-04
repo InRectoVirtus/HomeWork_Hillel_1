62de4bfee6ad95b7d8427c57b1d7112c927f9003
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2022-02-04 10:16:18 +0200

    Add: new exercise

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let radius = prompt('Введите радиус цилиндра');
-    height = prompt('Введите высоту цилиндра');
+// #1
+let radius = prompt('Введите радиус цилиндра'),
+    height = prompt('Введите высоту цилиндра'),
     volume = Math.PI * Math.pow(radius, 2) * height;
 
 document.write(`************** <br>
@@ -7,4 +8,24 @@ document.write(`************** <br>
 -------------------- <br>
 V = ${volume} <br>
 ------------------- <br>
-end.`)
+end.<br>`)
+
+// #2
+let firstNumber = +prompt('Введите первое число'),
+
+    secondNumber = +prompt('Введите второе число'),
+    thirdNumber = +prompt('Введите третье число');
+
+document.write(`Сумма всех чисел будет равна: ${firstNumber + secondNumber + thirdNumber}<br>`)
+
+if(firstNumber % 2 === 0) {
+    document.write(`${firstNumber} - чётное число <br>`)
+} 
+
+if (secondNumber % 2 === 0) {
+    document.write(`${secondNumber} - чётное число <br>`)
+} 
+
+if (thirdNumber % 2 === 0) {
+    document.write(`${thirdNumber} - чётное число <br>`)
+} 
