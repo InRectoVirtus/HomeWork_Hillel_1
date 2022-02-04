1efb3801dd2eb7cb089fc1ad950804d1b49f0ce1
InRectoVirtus <82666923+InRectoVirtus@users.noreply.github.com>
2022-02-04 19:12:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.write(`************** <br>
 -------------------- <br>
 V = ${volume} <br>
 ------------------- <br>
-end.<br>`)
+end.<br>`);
 
 // #2
 let firstNumber = +prompt('Введите первое число'),
@@ -16,16 +16,16 @@ let firstNumber = +prompt('Введите первое число'),
     secondNumber = +prompt('Введите второе число'),
     thirdNumber = +prompt('Введите третье число');
 
-document.write(`Сумма всех чисел будет равна: ${firstNumber + secondNumber + thirdNumber}<br>`)
+document.write(`Сумма всех чисел будет равна: ${firstNumber + secondNumber + thirdNumber}<br>`);
 
 if(firstNumber % 2 === 0) {
-    document.write(`${firstNumber} - чётное число <br>`)
+    document.write(`${firstNumber} - чётное число <br>`);
 } 
 
 if (secondNumber % 2 === 0) {
-    document.write(`${secondNumber} - чётное число <br>`)
+    document.write(`${secondNumber} - чётное число <br>`);
 } 
 
 if (thirdNumber % 2 === 0) {
-    document.write(`${thirdNumber} - чётное число <br>`)
+    document.write(`${thirdNumber} - чётное число <br>`);
 } 
